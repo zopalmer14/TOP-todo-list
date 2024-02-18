410316449bb97c1b1bfb9e59a8e0741f62c84a01
zopalmer14 <zopalmer14@gmail.com>
2024-02-17 21:12:57 -0500

    setup add_project form content, styling, and JS processing

--- dynamic.js
+++ dynamic.js
@@ -7,8 +7,9 @@ const scheduleController = function scheduleController() {
     const getProjects = () => projectList;
 
     // add project to list
-    const addProject = function addProject(project) {
-        projectList.push(project);
+    const addProject = function addProject(title) {
+        const new_project = createProject(title);
+        projectList.push(new_project);
     };
 
     // delete project from list
@@ -24,6 +25,8 @@ const scheduleController = function scheduleController() {
     // Project Factory Function (i.e. a list of to-do items)
     const createProject = function createProject(title) {
         const toDoList = [];
+
+        const getTitle = () => title;
 
         const getList = () => toDoList;
 
@@ -42,7 +45,7 @@ const scheduleController = function scheduleController() {
             }
         };
 
-        return { title, getList, addItem, deleteItem }
+        return { getTitle, getList, addItem, deleteItem }
     };
 
     // To-Do Item Factory Function 
@@ -50,15 +53,47 @@ const scheduleController = function scheduleController() {
         return { title, desc, dueDate, prio };
     };
 
-    return { getProjects, addProject, deleteProject, createProject, toDoItem };
+    return { getProjects, addProject, deleteProject, toDoItem, createProject };
 }();
 
 // DOM MANIPULATION
 
 const DOMController = function DOMController() {
     const setupSidebar = function setupSidebar() {
-        const nav_items = document.querySelectorAll('#nav>ul>li');
+        // dom references
+        const personal_projects = document.querySelector('#personal-projects');
 
+        // creates a list of the personal projects in the sidebar
+        function displayProjects() {
+            const projects = scheduleController.getProjects();
+
+            projects.forEach((project) => {
+                // create a list item to represent the project
+                const list_item = document.createElement('li');
+
+                // create a logo to add to the list item
+                const logo = document.createElement('span');
+                logo.classList.add('material-icons');
+                logo.textContent = 'list';
+
+                // create a div to hold the title
+                const title = document.createElement('div');
+                title.textContent = project.getTitle();;
+
+                // append both to the list item
+                list_item.appendChild(logo);
+                list_item.appendChild(title);
+
+                console.log(list_item);
+
+                // append the list item to the list of personal projects
+                personal_projects.appendChild(list_item);
+            });
+        }
+
+        displayProjects();
+
+        const nav_items = document.querySelectorAll('#nav>ul>li');
         nav_items.forEach((item) => {
             item.addEventListener('click', (event) => {
                 event.target.classList.add('active');
@@ -66,14 +101,34 @@ const DOMController = function DOMController() {
         });
     }
 
-    const setupAddTask = function setupAddTask() {
-        const add_task = document.querySelector('.add-item');
-        const dialog = document.querySelector('dialog');
-        const add_task_form = document.querySelector('#add-task-form'); 
+    const setupAddProject = function setupAddProject() {
+        // dom references
+        const add_project = document.querySelector('.add-project');
+        const add_project_dialog = document.querySelector('#add-project-dialog');
+        const add_project_form = document.querySelector('#add-project-form'); 
 
         // open the add project form when the user clicks the button
+        add_project.addEventListener('click', () => {
+            add_project_dialog.showModal();
+        });
+
+        // create a new project when the user submits the form
+        add_project_form.addEventListener('submit', (event) => {
+            const testItem = scheduleController.createProject(event.target.title.value);
+            console.log(testItem);
+            console.log(testItem.getTitle());
+        });
+    }
+
+    const setupAddTask = function setupAddTask() {
+        // dom references
+        const add_task = document.querySelector('.add-item');
+        const add_task_dialog = document.querySelector('#add-task-dialog');
+        const add_task_form = document.querySelector('#add-task-form'); 
+
+        // open the add task form when the user clicks the button
         add_task.addEventListener('click', () => {
-            dialog.showModal();
+            add_task_dialog.showModal();
         });
 
         // create a new task when the user submits the form
@@ -83,9 +138,14 @@ const DOMController = function DOMController() {
         });
     }
 
-    return { setupSidebar, setupAddTask};
+    return { setupSidebar, setupAddProject, setupAddTask};
 }();
 
+// back-end setup
+scheduleController.addProject('Testing');
+
+// front-end setup
 DOMController.setupSidebar();
 DOMController.setupAddTask();
+DOMController.setupAddProject();
 
